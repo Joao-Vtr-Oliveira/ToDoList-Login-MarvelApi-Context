c3becfa28d45fc573a861038d907026235d29984
João V. <joao.vtr.oliveira@outlook.com>
2023-12-30 14:08:20 -0300

    Add request function to save info

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,6 +3,24 @@ import { LoginContext } from "../contexts/LoginContext";
 import InputText from "./InputText";
 import Button from "./Button";
 
+const request = async (data: {user: string, password: string}) => {
+  const url = 'https://run.mocky.io/v3/49bf0d3b-a77b-4bad-8648-98e9560e12ef';
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    });
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 const Login = () => {
   const [user, setUser] = useState('');
   const [password, setPassword] = useState('');
@@ -17,6 +35,7 @@ const Login = () => {
     };
     loginContext.setInfos(user, password);
     loginContext.setIsLogged(true);
+    request({user, password});
   };
 
   const handleUserChange = (e: React.ChangeEvent<HTMLInputElement>) =>
