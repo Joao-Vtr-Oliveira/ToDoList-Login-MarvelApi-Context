378a49ad047345b9208093f21611b370f8dd0d5b
João V. <joao.vtr.oliveira@outlook.com>
2023-12-30 13:18:35 -0300

    feat: add the ...rest in the button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,8 @@
 import { ButtonType } from "../types/ButtonType"
 
-function Button({text, onClick, className}: ButtonType) {
+function Button({text, onClick, className, ...rest}: ButtonType) {
   return (
-    <button onClick={onClick} className={`text-white w-3/4 h-12 mt-0 font-mono font-bold rounded-md border border-gray-300 hover:border-custom-purple hover:italic ${className} xl:mt-0`}>{text}</button>
+    <button onClick={onClick} className={`text-white w-3/4 h-12 mt-0 font-mono font-bold rounded-md border border-gray-300 hover:border-custom-purple hover:italic ${className} xl:mt-0`} {...rest}>{text}</button>
   )
 }
 
