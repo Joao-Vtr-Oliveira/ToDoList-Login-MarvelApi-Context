d9d5e52cb52399c70f3c2c30740f82b29f63797d
João V. <joao.vtr.oliveira@outlook.com>
2024-01-08 19:38:14 -0300

    feat: add the LoginSvg component to deal the refresh problem

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
+import LoginSvg from "./LoginSvg";
 import Svg from "./Svg";
 
-//h-16
 function Header() {
   return (
     <div className="w-screen h-16 bg-gray-500 flex items-center justify-around">
       <div>
-        <Svg type="todo" target="_self" />
+        <LoginSvg />
       </div>
       <div className="flex justify-between w-44">
         <Svg type="github" />
--- src/types/SvgType.ts
+++ src/types/SvgType.ts
@@ -1,4 +1,4 @@
 export type svgType = {
-  type: 'linkedin' | 'reddit' | 'github' | 'todo';
-  target?: '_blank' | '_self';
+  type: 'linkedin' | 'reddit' | 'github';
+  target?: '_blank';
 }
